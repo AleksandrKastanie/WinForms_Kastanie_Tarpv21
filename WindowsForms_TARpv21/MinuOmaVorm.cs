8c6616b634405299b17ae3477c1be430c1fcd9f0
Alex Kastanie <roof10023@gmail.com>
2022-09-27 03:47:30 +0300

    Добавьте файлы проекта.

---
+++ WindowsForms_TARpv21/MinuOmaVorm.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_TARpv21
+{
+    public partial class MinuOmaVorm : Form
+    {
+        TreeView puu;
+        Button nupp;
+        Label silt;
+        CheckBox aruut1, aruut2;
+        RadioButton rnupp1, rnupp2, rnupp3, rnupp4;
+        PictureBox pilt;
+        ProgressBar rida;
+        Timer aeg;
+        TextBox tekst;
+        public MinuOmaVorm()
+        {
+            Height = 600;
+            Width = 900;
+            Text = "Minu oma vorm koos elementidega"; //название формы
+            puu= new TreeView();
+            puu.Dock = DockStyle.Left;
+            puu.Location = new Point(0,0);
+            TreeNode oksad= new TreeNode("Elemendid");
+            oksad.Nodes.Add(new TreeNode("Nupp-Button"));
+            oksad.Nodes.Add(new TreeNode("Silt-Label"));
+            oksad.Nodes.Add(new TreeNode("Dialog aken - MessageBox"));
+            oksad.Nodes.Add(new TreeNode("Märkeruut - Checkbox"));
+            oksad.Nodes.Add(new TreeNode("Radionupp"));
+            oksad.Nodes.Add(new TreeNode("ProgressBar"));
+            oksad.Nodes.Add(new TreeNode("Tekstkast - Textbox"));
+            oksad.Nodes.Add(new TreeNode("MinuVorm"));
+            //puu.AfterSelect += Puu_AfterSelect;
+            //puu.Nodes.Add(oksad);
+            //puu.DoubleClick += Tekst_DoubleClick;
+
+
+
+
+
+            puu.AfterSelect += Puu_AfterSelect;
+
+
+            puu.Nodes.Add(oksad);
+            this.Controls.Add(puu);
+
+
+            
+            BackColor = Color.White;
+
+        }
+
+        private void Puu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            silt = new Label
+            {
+                Text = "Minu esimene vorm",
+                Font = new Font("ArialBlack", 10),
+                Size = new Size(200, 50),
+                Location = new Point(150, 0)
+            };
+            aruut1 = new CheckBox
+            {
+                Checked = false,
+                Text = "TARpv21",
+                Location = new Point(silt.Left + silt.Width, 0),
+                Height = 25
+            };
+            aruut2 = new CheckBox
+            {
+                Checked = false,
+                Text = "LOGITpv21",
+                Location = new Point(silt.Left + silt.Width, aruut1.Height),
+                Height = 25
+            };
+            if (e.Node.Text == "Nupp-Button")
+            {
+                nupp = new Button();
+                nupp.Text = "Vajuta siia";
+                nupp.Height = 30;
+                nupp.Width = 100;
+                nupp.Location = new Point(200, 100);
+                nupp.Click += Nupp_Click;
+                this.Controls.Add(nupp);
+            }
+            else if (e.Node.Text == "Silt-Label")
+            {
+                silt = new Label
+                {
+                    Text = "Minu esimene vorm",
+                    Font = new Font("ArialBlack", 10),
+                    Size = new Size(200, 50),
+                    Location = new Point(450, 0)
+                };
+                silt.MouseEnter += Silt_MouseEnter;
+                silt.MouseLeave += Silt_MouseLeave;
+                this.Controls.Add(silt);
+            }
+            else if (e.Node.Text == "Dialog aken - MessageBox")
+            {
+
+                MessageBox.Show("Aken", "kõige lihtne aken");
+                var vastus = MessageBox.Show("kas paneme aken kinni?", "Valik",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (vastus == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                else { }
+            }
+            else if (e.Node.Text == "Märkeruut - Checkbox")
+            {
+                aruut1 = new CheckBox
+                {
+                    Checked = false,
+                    Text = "TARpv21",
+                    Location = new Point(silt.Left + silt.Width, 0),
+                    Height = 25
+                };
+                aruut2 = new CheckBox
+                {
+                    Checked = false,
+                    Text = "LOGITpv21",
+                    Location = new Point(silt.Left + silt.Width, aruut1.Height),
+                    Height = 25
+                };
+                aruut2.CheckedChanged += Aruut_1_2_Changed;
+                aruut1.CheckedChanged += Aruut_1_2_Changed;
+
+                this.Controls.Add(aruut1);
+                this.Controls.Add(aruut2);
+
+            }
+            else if (e.Node.Text == "Radionupp")
+            {
+                rnupp1 = new RadioButton
+                {
+                    Text = "Paremale",
+                    Width = 112,
+                    Location = new Point(aruut2.Left + aruut2.Width, aruut1.Height + aruut2.Height)
+                };
+                rnupp2 = new RadioButton
+                {
+                    Text = "Vasakule",
+                    Width = 112,
+                    Location = new Point(aruut2.Left + aruut2.Width+rnupp1.Width, aruut1.Height + aruut2.Height)
+                };
+                rnupp3 = new RadioButton
+                {
+                    Text = "Alla",
+                    Width = 112,
+                    Location = new Point(aruut2.Left + aruut2.Width+rnupp2.Width + rnupp1.Width, aruut1.Height + aruut2.Height)
+                };
+                rnupp4 = new RadioButton
+                {
+                    Text = "Üles",
+                    Width = 112,
+                    Location = new Point(aruut2.Left + aruut2.Width+rnupp3.Width+rnupp2.Width+rnupp1.Width, aruut1.Height + aruut2.Height)
+                };
+                pilt = new PictureBox
+                {
+                    Image=new Bitmap("b02.jpg"),
+                    Location = new Point(300,400),
+                    Size= new Size(100,200),
+                    SizeMode = PictureBoxSizeMode.Zoom
+
+                };
+                rnupp1.CheckedChanged += new EventHandler(Rnupud_Changed);
+                rnupp2.CheckedChanged += new EventHandler(Rnupud_Changed);
+                rnupp3.CheckedChanged += new EventHandler(Rnupud_Changed);
+                rnupp4.CheckedChanged += new EventHandler(Rnupud_Changed);
+
+                this.Controls.Add(pilt);
+                this.Controls.Add(rnupp1);
+                this.Controls.Add(rnupp2);
+                this.Controls.Add(rnupp3);
+                this.Controls.Add(rnupp4);
+
+            }
+            else if (e.Node.Text == "ProgressBar")
+            {
+                rida = new ProgressBar
+                {
+                    Width=400,
+                    Height=30,
+                    Location= new Point(300,450),
+                    Value=35,
+                    Minimum=0,
+                    Maximum=100,
+                    Step=1,
+                    //Dock=DockStyle.Bottom
+                };
+                aeg = new Timer();
+                aeg.Enabled = true;
+                aeg.Tick += Aeg_Tick;
+                this.Controls.Add(rida);
+
+            }
+            else if (e.Node.Text == "Tekstkast - Textbox")
+            {
+                tekst = new TextBox
+                {
+                    Font = new Font("ArialBlack", 34,FontStyle.Italic),
+                    Location = new Point(350, 400),
+                    Enabled = false,
+                };
+
+                //tekst.MouseDoubleClick += Tekst_DoubleClick;
+                this.Controls.Add(tekst);
+            }
+            else if (e.Node.Text=="MinuVorm")
+            {
+                MinuVorm minu = new MinuVorm("Kuulame muusikat");
+                minu.ShowDialog();
+
+
+            }
+        }
+
+        private void Tekst_DoubleClick(object sender, EventArgs e)
+        {
+            if (tekst.Enabled)
+            {
+                tekst.Enabled = false;
+            }
+            else
+            {
+                tekst.Enabled = true;
+            }
+        }
+
+        private void Aeg_Tick(object sender, EventArgs e)
+        {
+            rida.PerformStep();
+            MessageBox.Show( "See on Virus");
+        }
+
+
+
+        int x = 300;
+        int y = 300;
+        private void Rnupud_Changed(object sender, EventArgs e)
+        {
+            if(rnupp1.Checked)
+            {
+                x += 20;
+                pilt.Location = new Point(x,y);
+                rnupp1.Checked = false;
+            }
+            if (rnupp2.Checked)
+            {
+                x -= 20;
+                pilt.Location = new Point(x, y);
+                rnupp2.Checked = false;
+            }
+            if (rnupp3.Checked)
+            {
+                y += 20;
+                pilt.Location = new Point(x, y);
+                rnupp3.Checked = false;
+            }
+            if (rnupp4.Checked)
+            {
+                y -= 20;
+                pilt.Location = new Point(x, y);
+                rnupp4.Checked = false;
+            }
+        }
+
+
+        private void Aruut_1_2_Changed(object sender, EventArgs e)
+        {
+            if (aruut1.Checked == true && aruut2.Checked == true)
+            {
+                BackColor = Color.LightGreen;
+            }
+            else if (aruut1.Checked == true && aruut2.Checked == false)
+            {
+                BackColor = Color.DarkMagenta;
+            }
+            else if(aruut1.Checked == false && aruut2.Checked == true)
+            {
+                BackColor = Color.LightCyan;
+            }
+            else if(aruut1.Checked == false && aruut2.Checked == false)
+            {
+                BackColor = Color.DarkRed;
+            }
+        }
+
+
+        private void Silt_MouseEnter(object sender, EventArgs e)
+        {
+            silt.ForeColor = Color.White;
+            silt.BackColor = Color.Black;
+        }
+
+        private void Silt_MouseLeave(object sender, EventArgs e)
+        {
+            silt.ForeColor = Color.Black;
+            silt.BackColor = Color.Transparent;
+        }
+
+        Random random = new Random();
+        private void Nupp_Click(object sender, EventArgs e)
+        {
+            int red, green, blue;
+            red = random.Next(255);
+            green = random.Next(255);
+            blue= random.Next(255);
+
+            this.BackColor = Color.FromArgb(red,green,blue);
+        }
+
+    }
+}
---
+++ WindowsForms_TARpv21/MinuVorm.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_TARpv21
+{
+    public class MinuVorm : MinuOmaVorm
+    {
+
+        public MinuVorm() { }
+
+        public MinuVorm(string Pealkiri)
+        {
+
+            // Задаем размер окна и названик окна
+            this.ClientSize = new System.Drawing.Size(1300, 800);
+            this.Text = Pealkiri;
+
+            // Задаем кнопки 
+            // Перввая кнопка (картинка) - плей, запускает музыку  
+            Button play = new Button
+            {
+                Image = Image.FromFile(@"..\..\Play.png"),
+                Location = new System.Drawing.Point(635, 700),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.White,
+                FlatStyle = FlatStyle.Flat,
+            };
+            play.Click += Music_Click;
+
+            // Вторая кнопка остановки музыки
+            Button stop = new Button
+            {
+                Image = Image.FromFile(@"..\..\Stop.png"),
+                Location = new System.Drawing.Point(575, 700),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.White,
+                FlatStyle = FlatStyle.Flat
+            };
+            stop.Click += Stop_Click;
+
+            // третья кнопка запускает рандомную песню
+            Button randomS = new Button
+            {
+                Image = Image.FromFile(@"..\..\shuffle.png"),
+                Location = new System.Drawing.Point(690, 700),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.White,
+                FlatStyle = FlatStyle.Flat
+            };
+            randomS.Click += Random_Click;
+
+            // Первая песня 
+            Button song1 = new Button
+            {
+                Location = new System.Drawing.Point(200, 100),
+                Text = string.Format("1"),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.Red,
+            };
+            song1.Click += Nupp_Click1;
+
+            // Вторая песня
+            Button song2 = new Button
+            {
+                Location = new System.Drawing.Point(200, 175),
+                Text = string.Format("2"),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.Red,
+            };
+            song2.Click += Nupp_Click2;
+
+            // Третья песня
+            Button song3 = new Button
+            {
+                Location = new System.Drawing.Point(200, 250),
+                Text = string.Format("3"),
+                Size = new System.Drawing.Size(50, 50),
+                BackColor = System.Drawing.Color.Red,
+            };
+            
+            song3.Click += Nupp_Click3;
+
+            this.Controls.Add(song3);
+            this.Controls.Add(song2);
+            this.Controls.Add(song1);
+            this.Controls.Add(play);
+            this.Controls.Add(stop);
+            this.Controls.Add(randomS);
+
+            
+        }
+        public void Nupp_Click1(object sender, EventArgs e)
+        {
+            Button nupp_sender = (Button)sender;
+            var vastus = MessageBox.Show("Sa oled kindel et taha muusika kuulata?", "Küsimus", MessageBoxButtons.YesNo);
+            if (vastus == DialogResult.Yes)
+            {
+
+                using (var muusika = new SoundPlayer(@"..\..\1.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("Pumped up kicks");
+                    Label FailName = new Label
+                    {
+                        Text = string.Format(" - Pumped up kicks"),
+                        Location = new System.Drawing.Point(270, 110),
+                        Size = new System.Drawing.Size(100, 50),
+                        BackColor = System.Drawing.Color.White,
+                    };
+                    this.Controls.Add(FailName);
+                }           
+            }
+            else
+            {
+                MessageBox.Show(">_<");
+            }
+        }
+        public void Nupp_Click2(object sender, EventArgs e)
+        {
+            Button nupp_sender = (Button)sender;
+            var vastus = MessageBox.Show("Sa oled kindel et taha muusika kuulata?", "Küsimus", MessageBoxButtons.YesNo);
+            if (vastus == DialogResult.Yes)
+            {
+
+                using (var muusika = new SoundPlayer(@"..\..\2.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("MONTERO - LIL NAS");
+                    Label FailName = new Label
+                    {
+                        Text = string.Format(" - MONTERO - LIL NAS"),
+                        Location = new System.Drawing.Point(270, 185),
+                        Size = new System.Drawing.Size(100, 50),
+                        BackColor = System.Drawing.Color.White,
+                    };
+                    this.Controls.Add(FailName);
+                }
+            }
+            else
+            {
+                MessageBox.Show(">_<");
+            }
+        }
+        public void Nupp_Click3(object sender, EventArgs e)
+        {
+            Button nupp_sender = (Button)sender;
+            var vastus = MessageBox.Show("Sa oled kindel et taha muusika kuulata?", "Küsimus", MessageBoxButtons.YesNo);
+            if (vastus == DialogResult.Yes)
+            {
+
+                using (var muusika = new SoundPlayer(@"..\..\3.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("Life Goes On - Oliver Tree");
+                    Label FailName = new Label
+                    {
+                        Text = string.Format(" - Life Goes On - Oliver Tree"),
+                        Location = new System.Drawing.Point(270, 260),
+                        Size = new System.Drawing.Size(100, 50),
+                        BackColor = System.Drawing.Color.White,
+                    };
+                    this.Controls.Add(FailName);
+                }
+            }
+            else
+            {
+                MessageBox.Show(">_<");
+            }
+        }
+
+        public void Music_Click(object sender, EventArgs e)
+        {
+                using (var muusika = new SoundPlayer(@"..\..\1.wav"))
+                {
+                    muusika.Play();
+                }
+                using (var muusika = new SoundPlayer(@"..\..\2.wav"))
+                {
+                    muusika.Play();
+                }
+                using (var muusika = new SoundPlayer(@"..\..\3.wav"))
+                {
+                    muusika.Play();
+                }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            
+            using (var muusika = new SoundPlayer(@"..\..\1.wav"))
+            {
+                muusika.Stop();
+            }
+            using (var muusika = new SoundPlayer(@"..\..\2.wav"))
+            {
+                muusika.Stop();
+            }
+            using (var muusika = new SoundPlayer(@"..\..\3.wav"))
+            {
+                muusika.Stop();
+            }
+          
+            }
+
+        private void Random_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int b = rnd.Next(1, 4);
+            if (b == 1)
+            {
+                using (var muusika = new SoundPlayer(@"..\..\1.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("Pumped up kicks");
+                }
+            }
+            else if (b == 2)
+            {
+                using (var muusika = new SoundPlayer(@"..\..\2.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("MONTERO - LIL NAS");
+                }
+            }
+            else
+            {
+                using (var muusika = new SoundPlayer(@"..\..\3.wav"))
+                {
+                    muusika.Play();
+                    MessageBox.Show("Life Goes On - Oliver Tree");
+                }
+            }
+            //{
+            //    this.ClientSize = new System.Drawing.Size(700, 700);
+            //    this.Text = Pealkiri;
+
+            //    //первая песня
+            //    Button nupp = new Button
+            //    {
+            //        Text = Nupp,
+            //        Location = new System.Drawing.Point(150, 50),
+            //        Size = new System.Drawing.Size(100,50),
+            //        BackColor = System.Drawing.Color.YellowGreen,
+            //    };
+            //    nupp.Click += Nupp_Click;
+            //    Label failisnimi = new Label
+            //    {
+            //        Text = Fail,
+            //        Location = new System.Drawing.Point(150, 100),
+            //        Size = new System.Drawing.Size(100, 50),
+            //        BackColor = System.Drawing.Color.White,
+            //    };
+
+            //    //Вторая песня
+            //    Button nupp1 = new Button
+            //    {
+            //        Text = Nupp,
+            //        Location = new System.Drawing.Point(150, 50),
+            //        Size = new System.Drawing.Size(100, 50),
+            //        BackColor = System.Drawing.Color.Yellow,
+            //    };
+            //    nupp1.Click += Nupp_Click;
+            //    Label failisnimi1 = new Label
+            //    {
+            //        Text = Fail,
+            //        Location = new System.Drawing.Point(150, 100),
+            //        Size = new System.Drawing.Size(100, 50),
+            //        BackColor = System.Drawing.Color.White,
+            //    };
+
+            //    //Третья песня
+            //    Button nupp2 = new Button
+            //    {
+            //        Text = Nupp,
+            //        Location = new System.Drawing.Point(150, 50),
+            //        Size = new System.Drawing.Size(100, 50),
+            //        BackColor = System.Drawing.Color.Green,
+            //    };
+            //    nupp2.Click += Nupp_Click;
+            //    Label failisnimi2 = new Label
+            //    {
+            //        Text = Fail,
+            //        Location = new System.Drawing.Point(150, 100),
+            //        Size = new System.Drawing.Size(100, 50),
+            //        BackColor = System.Drawing.Color.White,
+            //    };
+            //    this.Controls.Add(nupp);
+            //    this.Controls.Add(failisnimi);
+            //    this.Controls.Add(nupp1);
+            //    this.Controls.Add(failisnimi1);
+            //    this.Controls.Add(nupp2);
+            //    this.Controls.Add(failisnimi2);
+            //}
+
+            //private void nupp_click(object sender, eventargs e)
+            //{
+            //    button nupp_sender = (button)sender;
+            //    var vastus = messagebox.show("kas tahad muusikat kuulata?", "küsimus",messageboxbuttons.yesno);
+            //    if (vastus == dialogresult.yes)
+            //    {
+            //        using (var muusika = new soundplayer(@"..\..\123.wav"))
+            //        {
+            //            var soundmessage = messagebox.show("playing: pumped up kicks");
+            //            muusika.play();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        messagebox.show(">_<");
+            //    }
+            //    this.controls.add(nupp_sender);
+            //}
+        }
+    }
+}
